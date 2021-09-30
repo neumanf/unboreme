4a0bddad7e5850cff38553e908a461161cc26851
neumanf <fabricionewman@protonmail.com>
2021-09-30 15:58:24 +0000

    fix: remove purge from tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,4 @@
 module.exports = {
-    purge: [
-        './resources/views/**/*.edge',
-        './resources/css/**/*.css',
-        './resources/js/**/*.js',
-        './resources/js/**/*.ts',
-    ],
     darkMode: false, // or 'media' or 'class'
     theme: {
         container: {
